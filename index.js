145cfc973d7f96b19bf7beea5949e150c98585d0
Paulo <pauloh2012sul@gmail.compwd>
2021-09-10 12:51:47 -0300

    Removed node_module folder

---
+++ index.js
@@ -0,0 +1,68 @@
+const express = require('express')
+const cors = require('cors');
+
+
+const app = express()
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(cors({
+    origin: ['https://www.spartacla.com.br', 'http://localhost:3000']
+}));
+
+const port = 3000
+
+app.post('/', function (req, res) {
+
+    //node index.js
+    //killall -9 node
+
+    const captchaAPIKey = 'e634119877d1596502fbdb9c13301f0d'
+
+
+     const api = require('call-of-duty-api')({ platform: req.body.platform, debug: 1 });
+
+    //const api = require('call-of-duty-api')();
+
+    try {
+
+        api.login(req.body.email, req.body.password, captchaAPIKey).then(start).catch(console.log);
+
+    } catch (Error) {
+
+        console.log('Error: ', Error)
+        //Handle Exception
+    }
+
+
+    async function start() {
+
+        try {
+
+            //STATS WARZONE
+            var statsWarzone = await api.MWBattleData(req.body.gamerTag);
+
+            let recentMatches = await api.MWcombatwz(req.body.gamerTag, req.body.platform);
+
+            const responseBody = {
+                status: 200,
+                gamerTag: req.body.gamerTag,
+                response: statsWarzone,
+                recentMatches: recentMatches,
+            }
+
+            res.status(200).send(responseBody);
+
+        } catch (Error) {
+
+            console.log('Error: ', Error)
+            //Handle Exception
+        }
+    }
+});
+
+app.listen(port, () => {
+    console.log(`app listening at http://localhost:${port}`)
+});
+
