c20f07e82272020db93797a31572b0a36ac883af
Paulo <pauloh2012sul@gmail.compwd>
2021-11-02 16:59:10 -0300

    sso token

--- login.js
+++ login.js
@@ -33,7 +33,11 @@ app.post('/login', function (req, res) {
         try {
 
             //STATS WARZONE
-           await api.login(req.body.email, req.body.password, captchaAPIKey).then(returnSSO).catch(console.log);
+            await api.login(req.body.email, req.body.password, captchaAPIKey).then(returnSSO).catch(console.log);
+
+            //api.apiAxios.defaults.headers.common.cookie = "XSRF-TOKEN=ydlRcTlIVLj8e5Can85f3PIRpyUBPjkR2fLZJpu-RPOVkW4QRdZ37uxv6gi2lrub;ACT_SSO_COOKIE=MTAwNTU1MTE2MzI1ODIxMjE0MDg6MTYzNzA5MTI1MDQ2NTo3MTUxZDk5ZDE4N2NiNjJkZDBjZDhjNjIzYTE2ZDAxMA;atkn=eyJhbGciOiAiQTEyOEtXIiwgImVuYyI6ICJBMTI4R0NNIiwgImtpZCI6ICJ1bm9fcHJvZF9sYXNfMSJ9.kyTZaytsujus0HH6D_1Oh30yx54WAbGGHXf0zEa9E6Nxs7aoLXftBg.SkG14aN5sli0ESom.TSz77jfRcidxONiyTSBa-wPV2gGrL8sHNIsqidg9DySXkfpdOCWd7Ccm4kALS6ZGnMJWXkmWJ4Bpg8ykhi5QCRe7hGS11NGbd4RtEcSTveTYqcSovDf1FAI7j7N6rQ2I8iitgld1n3lOl6vf4nw9GYOrQd-WQk1_fmLZG5w405N6ivx5nf7f5YM4oKThss9_N1cSMVQQqY0yZXDlmYF4tbG4hwsMVU5lAbEURsW-2dX8kAnK0ZYJIh-q1wvdbmGZz1bpMHjY8kWkxEc9cgFekL_bSgwSSsz3Mmd4wMgpVsU1UNonMSuFgcvC4LqMJnl9kot4UgnYZzN8TKSCAnHiL_T9pioruB_FE3KhNzX7vj4bu1DIDse71EYJFB5cJEzTG20YBr5x0O5LpzblqTl4kghZdmj7wik3kAlBLOcyPcsqyyhb4eVSZGZJrEopYL-_RP1srE-Kft-gJ_wbWbWK6zem5EwmAg._xNwzucm65uVw_MA0sS7kg;"
+
+            returnSSO();
 
         } catch (Error) {
 
@@ -46,16 +50,25 @@ app.post('/login', function (req, res) {
 
         try {
 
-            const SSOToken = api.apiAxios.defaults.headers.common.cookie;
+            let SSOToken = api.apiAxios.defaults.headers.common.cookie;
+
+            const searchEqual = '='
+            const replacerEqual = new RegExp(searchEqual, 'g')
+
+            SSOToken = SSOToken.replace(replacerEqual, ':')
+
+            SSOToken = SSOToken.split(":");
 
             const response = {
                 status: 200,
-                SSOToken: SSOToken
+                SSOToken: SSOToken[2].replace(";atkn", "")
             }
 
             res.status(200).send(response);
 
         } catch (Error) {
+
+            console.log(Error)
 
             res.status(401).send(Error);
 
